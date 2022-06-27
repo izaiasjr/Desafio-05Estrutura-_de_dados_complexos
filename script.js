60a7c972b6e76714d0d0fbe3d1f8545d2bfdb1d5
izaiasjr <up1.izaiasjr@gmail.com>
2022-06-27 00:52:57 -0300

    meu desafio de Estutura de dados complexos

---
+++ script.js
@@ -0,0 +1,36 @@
+// Crie um array que receba 5 itens e exiba no console.
+let times = ["Flamengo", "Milan", "Real Madrid", "Chelsea","Boca Juniors"]
+console.log (times)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+//uncshift()
+let paises = ["Brasil", "Argentina", "Alemanha"]
+paises.unshift("Portugal")
+console.log(paises)
+
+// Utilize um método para remover o último nome do array.
+//pop()
+let objetos = ["escada", "tesoura", "cadeira", "oculos"]
+objetos.pop()
+console.log(objetos)
+
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+//push()
+let jogador = ["Naymar","Hulk", "Cristiano Ronald",]
+jogador.push("Vinicus Jr", "Gabigol")
+console.log(jogador)
+
+// Utilize um método para remover o primeiro nome do array.
+//shift()
+let nomes = ["Gabriel", "Emerson", "Izaias", "David"]
+nomes.shift()
+console.log(nomes)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+// sort()
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
